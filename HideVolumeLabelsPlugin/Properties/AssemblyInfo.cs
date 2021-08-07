cb6bb8e9c96a842f51e77865100ef3b8d99291d7
Lord Ashes <webtable@gmail.com>
2021-08-07 00:39:35 -0400

    Version 1.0.1
    Updated version

--- HideVolumeLabelsPlugin/Properties/AssemblyInfo.cs
+++ HideVolumeLabelsPlugin/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RForRotatePlugin")]
+[assembly: AssemblyTitle("HideVolumeLabelsPlugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("D20ArmyKnife")]
 [assembly: AssemblyProduct("RForRotatePlugin")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("HideVolumeLabelsPlugin")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
